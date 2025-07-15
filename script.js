0966d88e98019723e5469a29a51efe043ea1d6cd
SamraKalwar <samraahmed569@gmail.com>
2025-07-15 19:56:35 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+let text = "I'm Samra Ahmed – Developer ✨ Designer ✨ Dreamer 💛";
+let i = 0;
+
+function typeWriter() {
+  if (i < text.length) {
+    document.getElementById("typewriter").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, 80);
+  }
+}
+window.onload = typeWriter;
+
+
+function showSkills() {
+  const skills = [
+    "💻 C++",
+    "☕ Java",
+    "🐍 Python",
+    "🌐 HTML/CSS",
+    "🗃️ SQL, MySQL, MongoDB",
+    "🧠 JavaScript",
+    "🎨 Canva, Figma",
+    "📊 PowerPoint, Word"
+  ];
+
+  const container = document.getElementById("skillsOutput");
+  container.innerHTML = "";
+  container.style.opacity = 0;
+  setTimeout(() => {
+    skills.forEach(skill => {
+      const box = document.createElement("div");
+      box.className = "skill-box";
+      box.innerText = skill;
+      container.appendChild(box);
+    });
+    container.style.opacity = 1;
+  }, 200);
+}
